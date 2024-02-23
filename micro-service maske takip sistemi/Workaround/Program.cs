5c9ccc444f02893d50f5bb1503cd09fca20783a2
Semanur <119792917+SemanurOrhan@users.noreply.github.com>
2024-02-24 02:53:23 +0300

    first.up

---
+++ micro-service maske takip sistemi/Workaround/Program.cs
@@ -0,0 +1,25 @@
+﻿using Business.Concrete;
+using Entities.Concreate;
+
+namespace Workaround
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Person person1 = new Person();
+            person1.FirstName = "SEMANUR";
+            person1.LastName = "ORHAN";
+            person1.DateOfBirthYear = 2004;
+            person1.NationalIdentity = 123;
+
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+
+
+
+        }
+    }
+}
